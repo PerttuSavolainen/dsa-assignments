c88c1eb6350460c367bb580f24f916373d6146f3
Perttu Savolainen <savolainen.perttu@gmail.com>
2017-02-16 05:46:11 +0000

    last fixes

--- assignment_2-5.js
+++ assignment_2-5.js
@@ -1,57 +1,61 @@
-function quickSort(arr, start, end) {
+/**
+ * Quick sort function,
+ * needs highest value to last array index,
+ * so Infinity is pushed before sorting and popped after 
+ */
+function quickSort(array, startIndex, endIndex) {
     
-    if (start < end) {
+    var sort = function(arr, start, end){
         
-        var i = start + 1;
-        var j = end + 1;
-        var destructor = arr[start];
+        if (start < end) {
         
-        while (i < j) {
+            var i = start + 1;
+            var j = end + 1;
+            var destructor = arr[start];
             
-            while (arr[i] < destructor) {
-                i++;
-            }    
+            while (i < j) {
                 
-            while (arr[j] > destructor) {
-                j--;
+                while (arr[i] < destructor) {
+                    i++;
+                }    
+                    
+                while (arr[j] > destructor) {
+                    j--;
+                }
+                
+                if (i < j) {
+                    var curr = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = curr;
+                    
+                    console.log("i: " + i);
+                    console.log("j: " + j);
+                }
+                
             }
+    
+            var tappi = j;
+            arr[start] = arr[tappi];
+            arr[tappi] = destructor;
             
-            if (i < j) {
-                var curr = arr[i];
-                arr[i] = arr[j];
-                arr[j] = curr;
-                
-                        
-                console.log("i: " + i);
-                console.log("j: " + j);
+            arr = sort(arr, start, tappi - 1);
+            arr = sort(arr, tappi + 1, end);
+            
+            return arr;
 
-                
-            }
-            
-        }
-        
-        
-        
-        var tappi = j;
-        arr[start] = arr[tappi];
-        arr[tappi] = destructor;
-        
-        arr = quickSort(arr, start, tappi - 1);
-        arr = quickSort(arr, tappi + 1, end);
-        
-        return arr;
-        
-    } else return arr;
+         } else return arr;
+    
+    }
+    
+    array.push(Infinity);
+    var sorted = sort(array, startIndex, endIndex);
+    sorted.pop();
+    
+    return sorted;
     
 }
 
-var array = [25, 50, 40, 20, 10, 15, 30, 35];
+var a = [25, 50, 40, 20, 10, 15, 30, 35];
+var b = [25, 50, 40, 20, 1, 867835,25, 50, 467567886, 15, 30, 35, 456456, 234, 7655767657667576576, 243324, 1, 2];
 
-var start = 0,
-    end = array.length-1;
-    
-array.push(Infinity);    
-var sorted = quickSort(array, start, end);
-sorted.pop();
-
-console.log(sorted);
+console.log(quickSort(a, 0, a.length-1));
